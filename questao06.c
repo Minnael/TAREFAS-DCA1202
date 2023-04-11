01b4a8bb9ca5532927b6f23cc9bc7f5908ab1037
Minnael <minnaelcampelo@gmail.com>
2023-04-10 21:18:45 -0300

    Adição da questão 06

---
+++ questao06.c
@@ -0,0 +1,10 @@
+/*
+   A expressão abaixo que referencia o terceiro elemento do vetor é a expressão *(pulo + 2), dado que *(pulo + i)
+   representa a seguinte condição x[i], dessa forma é possível afimar que *(pulo + 2) retorna x[2].
+
+*/
+
+*(pulo + 2); //CORRETA
+*(pulo + 4);
+pulo + 4;
+pulo + 2;
