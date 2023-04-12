ba6639f5e8e0bb8a330feb74ee07844a67ae523e
Minnael <minnaelcampelo@gmail.com>
2023-04-11 22:00:55 -0300

    Adição da questão 09

---
+++ questao09.c
@@ -0,0 +1,10 @@
+/*
+   Para char x[4]: x = 4092, x+1 = 4093, x+2 = 4094, x+3 = 4095
+
+   Para int x[4]: x = 4092, x+1 = 4094, x+2 = 4096, x+3 = 4098 
+   --> vale salientar que uma variável do tipo int ocupa 4 bytes na memória. 
+
+   Para float x[4]: x = 4092, x+1 = 4096, x+2 = 409A, x+3 = 409E
+
+   Para double x[4]: x = 4092, x+1 = 409A, x+2 = 40A2, x+3 = 40AA
+*/
